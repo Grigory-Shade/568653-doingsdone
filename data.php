95a203dfcc0a15e668fcc861fddd3babdbdd52b1
Grigory-Shade <33571244+Grigory-Shade@users.noreply.github.com>
2018-11-22 22:30:16 +0700

    Реализована шаблонизация проекта

---
+++ data.php
@@ -0,0 +1,38 @@
+<?php
+
+// показывать или нет выполненные задачи
+$show_complete_tasks = rand(0, 1);
+// массивы для категорий и задач
+$categories = ['Входящие', 'Учеба', 'Работа', 'Домашние дела', 'Авто'];
+$project = [
+['name' => 'Собеседование в IT компании',
+'date' => '01.12.2018',
+'categories' => '2',
+'complete' => false
+],
+['name' => 'Выполнить тестовое задание',
+'date' => '25.12.2018',
+'categories' => '2',
+'complete' => false
+],
+['name' => 'Сделать задание первого раздела',
+'date' => '21.12.2018',
+'categories' => '1',
+'complete' => true
+],
+['name' => 'Встреча с другом',
+'date' => '22.12.2018',
+'categories' => '0',
+'complete' => false
+],
+['name' => 'Купить корм для кота',
+'date' => 'Нет',
+'categories' => '3',
+'complete' => false
+],
+['name' => 'Заказать пиццу',
+'date' => 'Нет',
+'categories' => '3',
+'complete' => false
+]
+];
