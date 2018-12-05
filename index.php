eae5a3cd2f621d1639504714c2234add5cc06e59
Grigory-Shade <33571244+Grigory-Shade@users.noreply.github.com>
2018-12-06 03:06:29 +0700

    Реализована работа ссылок меню проектов

--- index.php
+++ index.php
@@ -17,6 +17,9 @@ else {
     $sql = 'SELECT id, name FROM categories WHERE user_id = ?';
     $result = mysqli_query($link, $sql);
     $categories = db_select_data($link, $sql, [$user_id]);
+    $sql = 'SELECT category_id FROM projects WHERE user_id = ?';
+    $result = mysqli_query($link, $sql);
+    $count_projects = db_select_data($link, $sql, [$user_id]);
     if (!$result) {
         $error = mysqli_error($link);
         $content = include_template('error.php', [
@@ -26,25 +29,33 @@ else {
     }
 }
 
-if (!$link) {
-    $error = mysqli_connect_error();
-    $content = include_template('error.php', [
-        'title' => 'Ошибка подключения',
-        'error' => $error
-    ]);
-}
-else {
-    $sql = 'SELECT status, name, file, period, category_id FROM projects WHERE user_id = ?';
-    $result = mysqli_query($link, $sql);
-    $project = db_select_data($link, $sql, [$user_id]);
-    if (!$result) {
-        $error = mysqli_error($link);
+if (isset($_GET['cat_id']))
+    $selected_category = $_GET['cat_id'];
+    if (!$link) {
+        $error = mysqli_connect_error();
         $content = include_template('error.php', [
             'title' => 'Ошибка подключения',
             'error' => $error
         ]);
+    } elseif (isset($_GET['cat_id'])) {
+        $sql = 'SELECT status, name, file, period, category_id FROM projects WHERE user_id = ? AND category_id = ?';
+    } elseif (!isset($_GET['cat_id'])) {
+        $sql = 'SELECT status, name, file, period, category_id FROM projects WHERE user_id = ?';
+    } elseif ($_GET['cat_id'] < 0) {
+        http_response_code(404);
+        echo 'Что-то пошло не так';
+        die();
     }
-}
+        $result = mysqli_query($link, $sql);
+        $project = db_select_data($link, $sql, [$user_id, $selected_category]);
+        if (!$result) {
+            $error = mysqli_error($link);
+            $content = include_template('error.php', [
+                'title' => 'Ошибка подключения',
+                'error' => $error
+            ]);
+        }
+
 
 $main_content = include_template('index.php', [
     'project' => $project,
@@ -54,8 +65,8 @@ $layout_content = include_template('layout.php', [
     'title' => 'Дела в порядке',
     'content' => $main_content,
     'categories' => $categories,
-    'project' => $project,
+    'count_projects' => $count_projects
 ]);
-//print_r ($categories);
-//print deadline($date_str);
+//print_r ($categories2);
+//print (number_of_tasks($categories, $catname['id']));
 print ($layout_content);
